563f141cb1eddde81f1fccb114b3342a81073914
AhmedTaha9792 <74019590+AhmedTaha9792@users.noreply.github.com>
2023-03-12 15:29:40 +0200

    Add Constrictors and Destructors

---
+++ assignment1/Population.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Ahmed Taha on 3/12/2023.
+//
+
+#include "population.h"
+
+Population::Population() {
+    size = 0;
+    chromosomes = nullptr;
+}
+
+Population::Population(Chromosome *chromosomes, int size) {
+    this->size = size;
+    chromosomes = new Chromosome[size];
+    for (int i = 0; i < size; ++i) {
+        chromosomes[i] = this->chromosomes[i];
+    }
+}
+
+Population::Population(Population &population) {
+    this->size = population.size;
+    population.chromosomes = new Chromosome[size];
+    for (int i = 0; i < size; ++i) {
+        population.chromosomes[i] = this->chromosomes[i];
+    }
+}
+
+Population::~Population() {
+    delete [] chromosomes;
+}
